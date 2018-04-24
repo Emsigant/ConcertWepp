ae72d1fe9b7d0ced6414e228a31d06e997deae63
emsigant <emsigant@qq.com>
2018-04-24 23:18:15 +0800

    20180424

--- pages/category/category.js
+++ pages/category/category.js
@@ -1,82 +1,86 @@
 // pages/category/category.js
-const mapTable = {
-	'0': {
-		text: '演唱会'
-	},
-	'1': {
-		text: '音乐剧'
-	},
-	'2': {
-		text: '音乐会'
-	}
-}
+const requestBaseIp = 'http://222.20.31.188:8080';
 const detailUrl = '/pages/detail/detail'
 Page({
 
-  /**
-   * 页面的初始数据
-   */
+	/**
+	 * 页面的初始数据
+	 */
 	data: {
-
+		pageNo: 1,
+		pageSize: 10,
 	},
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
+	/**
+	 * 生命周期函数--监听页面加载
+	 */
 	onLoad: function (options) {
-		let { itemId } = options
-		if (itemId) {
+		console.log(options);
+		let { title, showTypeId, location, } = options;
+		if (title && showTypeId) {
 			wx.setNavigationBarTitle({
-				title: mapTable[itemId].text
+				title,
+			});
+			wx.request({
+				url: requestBaseIp + '/show/queryListByType',
+				data: {
+					showTypeId,
+					location,
+					pageNo: this.data.pageNo,
+					pageSize: this.data.pageSize,
+				},
+				success: (resp) => {
+					console.log(resp)
+				},
 			})
 		}
 	},
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
+	/**
+	 * 生命周期函数--监听页面初次渲染完成
+	 */
 	onReady: function () {
-		
+
 	},
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
+	/**
+	 * 生命周期函数--监听页面显示
+	 */
 	onShow: function () {
 
 	},
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
+	/**
+	 * 生命周期函数--监听页面隐藏
+	 */
 	onHide: function () {
 
 	},
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
+	/**
+	 * 生命周期函数--监听页面卸载
+	 */
 	onUnload: function () {
-		
+
 	},
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
+	/**
+	 * 页面相关事件处理函数--监听用户下拉动作
+	 */
 	onPullDownRefresh: function () {
 
 	},
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
+	/**
+	 * 页面上拉触底事件的处理函数
+	 */
 	onReachBottom: function () {
 
 	},
 
-  /**
-   * 用户点击右上角分享
-   */
+	/**
+	 * 用户点击右上角分享
+	 */
 	onShareAppMessage: function () {
 
 	},
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,18 +1,67 @@
 //index.js
 //获取应用实例
 //app.globalData获取在app.js中定义的全局变量
+
 const app = getApp();
-const log = console.log;
+
+const requestBaseIp = 'http://222.20.31.188:8080';
 const categoryUrl = '/pages/category/category';
 const searchUrl = '/pages/search/search';
+const detailUrl = '/pages/detail/detail';
+
+const cimg1 = '/images/index/microphone.png';
+const cimg2 = '/images/index/dance.png';
+const cimg3 = '/images/index/music.png';
+
+const log = console.log;
+const _json = JSON.stringify;
+
+function queryIndex(el, data) {
+	if (el) {
+		wx.request({
+			url: requestBaseIp + '/show/queryIndex',
+			method: 'POST',
+			data,
+			success: function (res) {
+				console.log(res.data)
+			}
+		});
+	}
+}
+
+function queryIndexTypeList(el, data) {
+	if (el) {
+		wx.request({
+			url: requestBaseIp + '/show/queryIndexTypeList',
+			method: 'POST',
+			data,
+			success: function (res) {
+				console.log(res.data)
+			}
+		});
+	}
+}
+
+const hanldeQuery = (path, query) => {
+	let _query = '?'
+	for (let k in query) {
+		_query = _query + k + '=' + query[k] + '&';
+	}
+	_query = _query.substr(0, _query.length - 1);
+	return path + _query;
+};
 Page({
 	data: {
 		defaultCity: '武汉市',
 		defaultLocation: ['湖北省', '武汉市', '洪山区'],
+		topRecommendList: [],
+		recommentListType1: [],
+		recommentListType2: [],
+		recommentListType3: [],
 		bannerItemList: [
-			{ src: '/images/index/microphone.png', text: '演唱会' },
-			{ src: '/images/index/dance.png', text: '音乐剧' },
-			{ src: '/images/index/music.png', text: '音乐会' }
+			{ src: cimg1, text: '演唱会', path: categoryUrl, showTypeId: "STI0000000001", },
+			{ src: cimg2, text: '音乐剧', path: categoryUrl, showTypeId: "STI0000000002", },
+			{ src: cimg3, text: '音乐会', path: categoryUrl, showTypeId: "STI0000000003", },
 		],
 		recommendationList: {
 			col2: [
@@ -24,33 +73,130 @@ Page({
 				{ src: '/images/index/r4.jpg', title: 'title4' },
 				{ src: '/images/index/r5.jpg', title: 'title5' }
 			]
-		}
+		},
+		multiArray: [[], []],
+		multiIndex: [0, 0],
+		mapLocationToProvinceCity: {
+
+		},
+		map: [
+
+		],
+		cityFetched: false,
+		oldPro: 0,
+		oldCity: 0,
+		location: '',
 	},
 
 	onLoad: function () {
-
-	},
-	//eventhandlers
-	chooseLocation(e) {
-		log('choose location')
+		// 获取城市列表
+		wx.request({
+			url: requestBaseIp + '/show/queryLocation',
+			method: 'POST',
+			success: (resp) => {
+				let arr = resp.data.content.dataList,
+					_map = this.data.mapLocationToProvinceCity,
+					_multiArray = this.data.multiArray,
+					map = this.data.map;
+				let l = arr.length;
+				for (let i = 0; i < l; i++) {
+					let pro = arr[i].province,
+						city = arr[i].city;
+					if (pro in _map) {
+						_map[pro].push(city);
+					} else {
+						_map[pro] = [city];
+					}
+				}
+				for (let key in _map) {
+					_multiArray[0].push(key);
+					map.push({
+						province: key,
+						city: _map[key],
+					});
+				}
+				for (let i = 0; i < map[this.data.oldPro].city.length; i++) {
+					_multiArray[1].push(map[this.data.oldPro].city[i]);
+				}
+				this.setData({
+					mapLocationToProvinceCity: _map,
+					multiArray: _multiArray,
+					map,
+					cityFetched: true,
+				});
+				// 根据地点请求首页推荐
+				let location = this.data.multiArray[0][this.data.oldPro] + '|' + this.data.multiArray[1][this.data.oldCity];
+				queryIndex(this, { location });
+				queryIndexTypeList(this, { location });
+			},
+			fail: (resp) => {
+				console.log(resp);
+			}
+		});
 	},
 	tapItem(e) {
 		// console.log(e);
 		let _id = e.currentTarget.dataset.itemId;
 		wx.navigateTo({
-			url: categoryUrl + '?from=index&itemId=' + _id
+			url: categoryUrl + '?from=index&itemId=' + _id,
 		});
 	},
 	goToSearch() {
 		log('go to search')
 		wx.navigateTo({
-			url: searchUrl + '?from=index'
+			url: searchUrl + '?from=index',
 		})
 	},
 	bindRegionChange(e) {
 		let v = e.detail.value
 		this.setData({
-			defaultLocation: [v.shift(), v.shift(), v.shift()]
+			defaultLocation: [v.shift(), v.shift(), v.shift()],
 		})
+	},
+	goDetail(e) {
+		let _path = e.currentTarget.dataset.path
+		wx.navigateTo({
+			url: _path
+		})
+	},
+	bindMultiPickerChange: function (e) {
+		this.setData({
+			multiIndex: [e.detail.value[0], e.detail.value[1]],
+			oldPro: e.detail.value[0],
+			oldCity: e.detail.value[1],
+		});
+		let location = this.data.multiArray[0][this.data.oldPro] + '|' + this.data.multiArray[1][this.data.oldCity];
+		queryIndex(this, { location });
+		queryIndexTypeList(this, { location });
+	},
+	bindMultiPickerColumnChange: function (e) {
+		let _column = e.detail.column,
+			_value = e.detail.value,
+			map = this.data.map;
+		let _multiArray = this.data.multiArray;
+		if (_column === 0) {
+			this.setData({
+				multiArray: [_multiArray[0], map[_value].city],
+				multiIndex: [_value, 0],
+			})
+		}
+	},
+	bindMultiPickerCancel(e) {
+		let _ma = this.data.multiArray,
+			_mltpc = this.data.mapLocationToProvinceCity,
+			_op = this.data.oldPro;
+		this.setData({
+			multiArray: [_ma[0], _mltpc[_ma[0][_op]]],
+			multiIndex: [_op, this.data.oldCity],
+		})
+	},
+	bindTap(e) {
+		let ctds = e.currentTarget.dataset;
+		let { path, showTypeId, title, location, } = ctds;
+		if (path) {
+			wx.navigateTo({
+				url: hanldeQuery(path, { showTypeId, title, location, }),
+			});
+		}
 	}
 })
